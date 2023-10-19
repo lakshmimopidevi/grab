9abdb847d6f95b26a4fda241d870672b66af4f51
lakshmimopidevi <125748752+lakshmimopidevi@users.noreply.github.com>
2023-10-19 15:31:32 +0530

    Update config.js

--- server/utils/config.js
+++ server/utils/config.js
@@ -5,7 +5,7 @@ dotenv.config();
 export const Server = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || "development",
-    MONGO_URI: process.env.MONGO_URI || "mongodb://localhost:27017/grabbit",
+    MONGO_URI: process.env.MONGO_URI || "mongodb://172.31.31.130:27017/grabbit",
     JWT_SECRET: process.env.JWT_SECRET || "cZNBhV5EMOeJjqp4y3Of3JGVb0+DfMfUK6SeAiLmqwI=",
     EMAIL: process.env.EMAIL || "gracie79@ethereal.email",
     PASSWORD: process.env.PASSWORD || "P5adXq1UFrNBKUUfWd"
